a5f6f0cf3058d91cf8e3d0cac5d9ae08f50f5635
xenosign <xenosign@naver.com>
2023-04-10 17:55:09 +0900

    멀티 전송 구현

--- src/routes/file.js
+++ src/routes/file.js
@@ -13,13 +13,15 @@ const storage = multer.diskStorage({
   },
 });
 const limits = {
-  fileSize: 1024 * 1028 * 2,
+  fileSize: 1024 * 1024 * 2,
 };
 
 const upload = multer({ storage, limits });
 
-router.post('/', upload.single('img'), async (req, res) => {
+router.post('/', upload.array('img'), async (req, res) => {
   if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+  console.log(req.body);
 
   const registerInfo = req.body;
   res.send(JSON.stringify(registerInfo));
